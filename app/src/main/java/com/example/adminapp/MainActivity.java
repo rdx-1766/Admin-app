8220d4f8392f2b7cd46bc514197d42d5d313a130
rdx-1766 <pavankumarmattad@gmail.com>
2023-05-10 23:52:56 +0530

    11th done

--- app/src/main/java/com/example/adminapp/MainActivity.java
+++ app/src/main/java/com/example/adminapp/MainActivity.java
@@ -10,9 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.adminapp.faculty.UpdateFaculty;
+
 public class MainActivity extends AppCompatActivity {
 
-    CardView uploadNotice,addGalleryImage;
+    CardView uploadNotice,addGalleryImage,addEbook,addFaculty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         addGalleryImage = findViewById(R.id.addGalleryImage);
         uploadNotice = findViewById(R.id.addNotice);
+        addEbook = findViewById(R.id.addEbook);
+        addFaculty = findViewById(R.id.addFaculty);
 
         uploadNotice.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, UploadImage.class);
+                startActivity(intent);
+            }
+        });
+
+        addEbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UploadPdf.class);
+                startActivity(intent);
+            }
+        });
+
+        addFaculty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UpdateFaculty.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/adminapp/UploadImage.java
+++ app/src/main/java/com/example/adminapp/UploadImage.java
@@ -1,9 +1,11 @@
 package com.example.adminapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -15,7 +17,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class UploadImage extends AppCompatActivity {
@@ -27,12 +41,19 @@ public class UploadImage extends AppCompatActivity {
     private String category;
     private final int REQ = 1;
     private Bitmap bitmap;
+    ProgressDialog pd;
+    private DatabaseReference reference;
+    private StorageReference storageReference;
+    String downloadUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
 
+        reference = FirebaseDatabase.getInstance().getReference().child("gallery");
+        storageReference = FirebaseStorage.getInstance().getReference().child("gallery");
+        pd = new ProgressDialog(this);
         selectImage = findViewById(R.id.addGalleryImage);
         imageCategory = findViewById(R.id.image_category);
         uploadImage= findViewById(R.id.uploadImageBtn);
@@ -56,6 +77,72 @@ public class UploadImage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openGallery();
+            }
+        });
+
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bitmap ==null){
+                    Toast.makeText(UploadImage.this, "Please Upload image", Toast.LENGTH_SHORT).show();
+                } else if (category.equals("Select Category")) {
+                    Toast.makeText(UploadImage.this, "Please select category", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    pd.setMessage("Uploading...");
+                    pd.show();
+                    uploadImage();
+                }
+            }
+        });
+    }
+
+    private void uploadImage() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,50,baos);
+        byte[] finalimg = baos.toByteArray();
+        final StorageReference filePath;
+        filePath = storageReference.child(finalimg+"jpg");
+        final UploadTask uploadTask = filePath.putBytes(finalimg);
+        uploadTask.addOnCompleteListener(UploadImage.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadUrl = String.valueOf(uri);
+                                    uploadData();
+                                }
+                            });
+                        }
+                    });
+                }else {
+                    pd.dismiss();
+                    Toast.makeText(UploadImage.this,"something went wrong",Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
+
+    private void uploadData() {
+        reference = reference.child(category);
+        final String uniqueKey = reference.push().getKey();
+
+        reference.child(uniqueKey).setValue(downloadUrl).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                pd.dismiss();
+                Toast.makeText(UploadImage.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(UploadImage.this, "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
